dd12a561c9c61893ad8758a381b5a3e8cf913e09
Ali Syed <52847195+GoodMorningA1i@users.noreply.github.com>
2025-02-15 15:24:45 -0500

    fixed incorrect docstring

--- reward_functions/in_state_reward.py
+++ reward_functions/in_state_reward.py
@@ -2,21 +2,10 @@ def in_state_reward(
     env: WarehouseBrawl,
     desired_state: Type[PlayerObjectState]=BackDashState,
 ) -> float:
-    """
-    Applies a penalty for every time frame player surpases a certain height threshold in the environment.
-
-    Args:
-        env (WarehouseBrawl): The game environment.
-        zone_penalty (int): The penalty applied when the player is in the danger zone.
-        zone_height (float): The height threshold defining the danger zone.
-
-    Returns:
-        float: The computed penalty as a tensor.
-    """
     # Get player object from the environment
     player: Player = env.objects["player"]
 
-    # Apply penalty if the player is in the danger zone
+    # Apply reward if the player is in the desired state
     reward = 1 if isinstance(player.state, desired_state) else 0.0
 
     return reward * env.dt
