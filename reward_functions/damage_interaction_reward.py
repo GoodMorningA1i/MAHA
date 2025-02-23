04fe19a93a7a69a25a29340a4bb05cd1c93688e4
madhavajayamohan <madhavajayamohan@gmail.com>
2025-02-23 11:13:09 -0500

    Modified Damage Interac

---
+++ reward_functions/damage_interaction_reward.py
@@ -0,0 +1,37 @@
+def damage_interaction_reward(
+    env: WarehouseBrawl,
+    mode: RewardMode = RewardMode.SYMMETRIC,
+) -> float:
+    """
+    Computes the reward based on damage interactions between players.
+
+    Modes:
+    - ASYMMETRIC_OFFENSIVE (0): Reward is based only on damage dealt to the opponent
+    - SYMMETRIC (1): Reward is based on both dealing damage to the opponent and avoiding damage
+    - ASYMMETRIC_DEFENSIVE (2): Reward is based only on avoiding damage
+
+    Args:
+        env (WarehouseBrawl): The game environment
+        mode (DamageRewardMode): Reward mode, one of DamageRewardMode
+
+    Returns:
+        float: The computed reward.
+    """
+    # Getting player and opponent from the enviornment
+    player: Player = env.objects["player"]
+    opponent: Player = env.objects["opponent"]
+
+    # Reward dependent on the mode
+    damage_taken = player.damage_taken_this_frame
+    damage_dealt = opponent.damage_taken_this_frame
+
+    if mode == RewardMode.ASYMMETRIC_OFFENSIVE:
+        reward = damage_dealt
+    elif mode == RewardMode.SYMMETRIC:
+        reward = damage_dealt - damage_taken
+    elif mode == RewardMode.ASYMMETRIC_DEFENSIVE:
+        reward = -damage_taken
+    else:
+        raise ValueError(f"Invalid mode: {mode}")
+
+    return reward / 140
