fc59af329551d19b6fefc1bf1cd0042fcdc981eb
Ali Syed <52847195+GoodMorningA1i@users.noreply.github.com>
2025-02-24 18:12:17 -0500

    added UserInputAgent with a few changes

---
+++ user_input_agent.py
@@ -0,0 +1,39 @@
+import pygame
+
+class UserInputAgent(Agent):
+
+    def __init__(
+            self,
+            *args,
+            **kwargs
+    ):
+        super().__init__(*args, **kwargs)
+        pygame.init()
+        self.screen = pygame.display.set_mode((640, 480))
+
+    def predict(self, obs):
+        action = self.act_helper.zeros()
+
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_w]:
+            action = self.act_helper.press_keys(['w'], action)
+        if keys[pygame.K_a]:
+            action = self.act_helper.press_keys(['a'], action)
+        if keys[pygame.K_s]:
+            action = self.act_helper.press_keys(['s'], action)
+        if keys[pygame.K_d]:
+            action = self.act_helper.press_keys(['d'], action)
+        if keys[pygame.K_SPACE]:
+            action = self.act_helper.press_keys(['space'], action)
+        # h j k l
+        if keys[pygame.K_h]:
+            action = self.act_helper.press_keys(['h'], action)
+        if keys[pygame.K_j]:
+            action = self.act_helper.press_keys(['j'], action)
+        if keys[pygame.K_k]:
+            action = self.act_helper.press_keys(['k'], action)
+        if keys[pygame.K_l]:
+            action = self.act_helper.press_keys(['l'], action)
+        if keys[pygame.K_g]:
+            action = self.act_helper.press_keys(['g'], action)
+        return action
